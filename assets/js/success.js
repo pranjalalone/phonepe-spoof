69d3a3712b9bce97bcdd3bf4a79641a0c75cf99c
PranjalAlone <pranjalalone@gmail.com>
2020-02-16 03:45:38 +0530

    Added Date Feature

--- assets/js/success.js
+++ assets/js/success.js
@@ -1,4 +1,17 @@
 window.onload = () => {
+    const getDate = () => {
+        let dateString = Date()
+        let dateArray = dateString.split(' ');
+        let date = dateArray[2]
+        let month = dateArray[1]
+        let year = dateArray[3]
+        let rawTimeArray = dateArray[4].split(':');
+        let time = (parseInt(rawTimeArray[0], 10) > 12) ? ("0" + (parseInt(rawTimeArray, 10) - 12) + ":" + rawTimeArray[1]) + " PM" : (rawTimeArray[0] + ":" + rawTimeArray[1] + " AM");
+        let completeDate = time +  " on " + date + " " + month + " " + year;
+        // console.log(completeDate);
+        return completeDate;
+    }
+
     let url = document.location.href;
     // console.log(url);
     let params = url.split('?')[1].split('&');
@@ -16,7 +29,10 @@ window.onload = () => {
     const name = document.getElementById('name');
     const amount = document.querySelector('#amount span');
     const bankAmt = document.querySelector('#bank-amt span');
+    const dateTime = document.getElementById('date-time');
     name.innerHTML = data.name;
     amount.innerHTML = data.amount;
     bankAmt.innerHTML = data.amount;
+
+    dateTime.innerHTML = getDate();
 }
