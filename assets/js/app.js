ee94182fe56739e8fd9831454d5ca64939f0590d
PranjalAlone <pranjalalone@gmail.com>
2020-02-16 02:59:19 +0530

    Fixed Redirect Issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,6 @@ const name = document.getElementById('merchant-name');
 const amount = document.getElementById('merchant-amount');
 
 payBtn.addEventListener('click', () => {
-    url = 'phonepe-spoof/redirects/paymentsuccess.html?name=' + name.value + '&amount=' + amount.value;
+    url = '/paymentsuccess.html?name=' + name.value + '&amount=' + amount.value;
     document.location.href = url;
 });
