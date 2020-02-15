36a347cc9b5fbbab970e966d2a27d674d48a09a8
PranjalAlone <pranjalalone@gmail.com>
2020-02-16 03:20:36 +0530

    Fixed Redirect Issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,13 @@ const name = document.getElementById('merchant-name');
 const amount = document.getElementById('merchant-amount');
 
 payBtn.addEventListener('click', () => {
-    url = document.location.href + 'paymentsuccess.html?name=' + name.value + '&amount=' + amount.value;
+    
+    if ((document.location.href.indexOf('localhost') === -1) || (document.location.href.indexOf('127.0.0.1') === -1)) {
+        // Development
+        url = 'paymentsuccess.html?name=' + name.value + '&amount=' + amount.value;
+    } else {
+        // Production
+        url = '/paymentsuccess.html?name=' + name.value + '&amount=' + amount.value;
+    }
     document.location.href = url;
 });
